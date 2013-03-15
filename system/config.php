e66d157db0e5eeca2dec46d0d7d2be1c4331e4a7
Lukas Epple <post@lukasepple.de>
2013-03-15 16:01:52 +0100

    Changed blog name

--- system/config.php
+++ system/config.php
@@ -4,5 +4,5 @@ define("POST_DIR", "posts");
 //Where is your installation located
 define("BASE_URL", "http://192.168.1.17/~lukasepple/blog/");
 //Your blog's name
-define("BLOG_NAME", "Hello World Blog");
+define("BLOG_NAME", "Blog – Markdown blogging engine");
 ?>
