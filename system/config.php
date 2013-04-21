22e9894c3992124412df31ea06f0c67f91e35293
lukasepple <lukas.epple@me.com>
2013-04-21 17:13:46 +0200

    Added settings.php
    Now you can change your configuration using the settings.php

--- system/config.php
+++ system/config.php
@@ -1,14 +1,15 @@
-<?php
-//Where are your posts
+<?php //Where are your posts
 define("POST_DIR", "posts");
 //Where is your installation located
 define("BASE_URL", "http://localhost/p/mbe/");
 //Your blog's name
-define("BLOG_NAME", "Markdown blogging engine");
+define("BLOG_NAME", "markdown blogging engine");
 //Your Template
 define("TEMPLATE", "simple");
+//Password for settings.php
+define("BLOG_PASSWORD", "42");
 //Twitter account
 define("TWITTER", "lukasepple");
-//Mail 
+//Mail
 define("MAIL", "post@lukasepple.de");
 ?>
