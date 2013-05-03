d096eb9ff357ef8dde4cd4e8c3b8c17595cc3865
Lukas Epple <lukas.epple@me.com>
2013-05-03 17:24:34 +0300

    Added support for not relative POST_DIRs

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("system/classes.php");
 $posts = new Posts();
 if(isset($_GET['q'])){
 	$query = rawurldecode(trim($_GET['q']));
-	if(file_exists("./" . POST_DIR . "/" . $query)){
+	if(file_exists(POST_DIR . "/" . $query)){
 		$post = new Post($query);
 		require_once("template/".TEMPLATE."/header.php");
 		require_once("template/".TEMPLATE."/single.php");
